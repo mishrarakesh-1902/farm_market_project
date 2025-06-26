15576b3fe4c849c90181eac035a232b819985da2
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-06-27 03:07:28 +0530

    Initial commit of Django project

--- marketplace/views.py
+++ marketplace/views.py
@@ -117,7 +117,7 @@ def add_to_cart(request, product_id):
         cart_item.quantity = 1
     
     cart_item.save()
-    return redirect('cart')
+    return redirect('product_list')
 
 @login_required
 def cart(request):
