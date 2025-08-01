1228a81aa5cc0b9fc86fea15c8e53457e8c39603
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-08-01 23:04:22 +0530

    change db

--- farm_market/settings.py
+++ farm_market/settings.py
@@ -95,14 +95,11 @@ WSGI_APPLICATION = 'farm_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 AUTH_PROFILE_MODULE = 'marketplace.UserProfile'
 
